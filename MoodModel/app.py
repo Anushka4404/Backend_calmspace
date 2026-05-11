66fd52ffea0ef47f6b8fe030c25ae6b0d7755483
Anushka Sharma <anushka4404@gmail.com>
2026-05-12 03:18:42 +0530

    Updated app.py

--- MoodModel/app.py
+++ MoodModel/app.py
@@ -96,5 +96,8 @@ def predict_emotion():
 
 
 if __name__ == '__main__':
+    import os
     print("🚀 ML Server running on 5000")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
