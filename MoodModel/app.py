6249e8482103638c23edef0da3dda01b683dcafa
Anushka Sharma <anushka4404@gmail.com>
2026-05-12 17:38:08 +0530

    Fixed CORS issue

--- MoodModel/app.py
+++ MoodModel/app.py
@@ -115,6 +115,12 @@ def predict_emotion():
 
 if __name__ == '__main__':
     import os
-    print("🚀 ML Server running on 5000")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+
+    print(f"🚀 ML Server running on port {port}")
+
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False
+    )
